0e2603499eaf6dcac385f18ba64b4285cad2f02b
Zhang Hai <dreaming.in.code.zh@gmail.com>
2015-07-08 12:18:55 +0800

    Integrate ALU.

--- ALU.v
+++ ALU.v
@@ -1,93 +1,36 @@
 `timescale 1ns / 1ps
 
 module ALU(
-		A,
-		ALU_operation,
-		B,
-		overflow,
-		res,
-		zero
+		input [31:0] A,
+		input [2:0] ALU_operation,
+		input [31:0] B,
+		output overflow,
+		output [31:0] res,
+		output zero
 	);
 
-	input [31:0] A;
-	input [2:0] ALU_operation;
-	input [31:0] B;
-	output overflow;
-	output [31:0] res;
-	output zero;
-
 	wire [31:0] B_Ctrl;
-	wire N0;
 	wire [32:0] S;
-	wire [31:0] XLXN_8;
-	wire [31:0] XLXN_12;
-	wire [31:0] XLXN_13;
-	wire [31:0] XLXN_17;
-	wire [31:0] XLXN_18;
-	wire [31:0] XLXN_19;
-	wire [31:0] res_DUMMY;
 
-	assign res[31:0] = res_DUMMY[31:0];
+	assign B_Ctrl = {32{ALU_operation[2]}};
 	adc_32 ALU_ADC (
-			.A(A[31:0]),
-			.B(XLXN_8[31:0]),
+			.A(A),
+			.B(B_Ctrl ^ B),
 			.C0(ALU_operation[2]),
 			.overflow(overflow),
-			.S(S[32:0])
+			.S(S)
 	);
 	mux8to1_32 ALU_MUX (
 			.sel(ALU_operation[2:0]),
-			.x0(XLXN_12[31:0]),
-			.x1(XLXN_13[31:0]),
+			.x0(A & B),
+			.x1(A | B),
 			.x2(S[31:0]),
-			.x3(XLXN_17[31:0]),
-			.x4(XLXN_18[31:0]),
-			.x5(XLXN_19[31:0]),
+			.x3(A ^ B),
+			.x4(~(A | B)),
+			.x5(B >> A[4:0]),
 			.x6(S[31:0]),
-			.x7({N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0,
-			N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0,
-			N0, S[32]}),
-			.o(res_DUMMY[31:0])
+			.x7({31'b0, S[32]}),
+			.o(res)
 	);
-	and_32 ALU_U1 (
-			.A(A[31:0]),
-			.B(B[31:0]),
-			.res(XLXN_12[31:0])
-	);
-	or_32 ALU_U2 (
-			.A(A[31:0]),
-			.B(B[31:0]),
-			.res(XLXN_13[31:0])
-	);
-	xor_32 ALU_U3 (
-			.A(A[31:0]),
-			.B(B[31:0]),
-			.res(XLXN_17[31:0])
-	);
-	nor_32 ALU_U4 (
-			.A(A[31:0]),
-			.B(B[31:0]),
-			.res(XLXN_18[31:0])
-	);
-	srl_32 ALU_U5 (
-			.A(A[31:0]),
-			.B(B[31:0]),
-			.res(XLXN_19[31:0])
-	);
-	xor_32 ALU_U7 (
-			.A(B_Ctrl[31:0]),
-			.B(B[31:0]),
-			.res(XLXN_8[31:0])
-	);
-	nor_bit_32 ALU_Zero (
-			.A(res_DUMMY[31:0]),
-			.o(zero)
-	);
-	SignalExt_32 Signal1_32 (
-			.S(ALU_operation[2]),
-			.So(B_Ctrl[31:0])
-	);
-	GND XLXI_22 (
-			.G(N0)
-	);
+	assign zero = ~|res;
 endmodule
--- SignalExt_32.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-
-module SignalExt_32(
-		input S,
-		output [31:0] So
-		);
-	assign So = {32{S}};
-endmodule
